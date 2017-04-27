2e6b5fc9e1ed4944e433ea80c1c9b23e3dfa5ce8
claytoneast <claytoneast@gmail.com>
2017-04-27 16:15:56 -0700

    updates readme, adds pip requirements file, gitignore, env, example env

--- FestivalMapAPI.py
+++ FestivalMapAPI.py
@@ -4,6 +4,14 @@ Created on Sun Apr 09 19:52:56 2017
 
 @author: Garrett
 """
+# import os
+# from dotenv import load_dotenv, find_dotenv
+# load_dotenv(find_dotenv())
+#
+# DB_NAME = os.environ.get("DB_NAME")
+# DB_USERNAME = os.environ.get("DB_USERNAME")
+# DB_URL = os.environ.get("DB_URL")
+# DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 from flask import Flask, jsonify, request, json, Response
 from flask_cors import CORS, cross_origin
@@ -11,42 +19,44 @@ app = Flask(__name__)
 CORS(app)
 import psycopg2
 
+# fullDbUrl = "dbname={DB_NAME} user={DB_USERNAME} host={DB_URL} password={DB_PASSWORD}".format(DB_NAME=DB_NAME)
+
 try:
     conn = psycopg2.connect("dbname='FestivalMap' user='festivalmap' host='sportsdatabases.cki9udpbxnex.us-west-2.rds.amazonaws.com' password='festivalmap'")
 except:
     print "I am unable to connect to the database"
-    
+
 cur = conn.cursor()
 
 @app.route('/festivals', methods=['GET'])
 def getFestivals():
     args = request.args.getlist('artist')
-    
+
 #    print artistList
-    
+
     # Query the database for any festival that has one of the artists entered
-    cur.execute("""SELECT 
-				f.latitude,
-				f.longitude,
+    cur.execute("""SELECT
+                f.latitude,
+                f.longitude,
             f.name,
-    			f.start_date,
+                f.start_date,
             f.end_date,
-			-- Get a complete list of artists for each festival		
+            -- Get a complete list of artists for each festival
             (SELECT json_agg(artists.name) FROM artists JOIN festivalartists ON artists.artistid = festivalartists.artistid
-			WHERE festivalartists.festivalid = f.festivalid) as artists
+            WHERE festivalartists.festivalid = f.festivalid) as artists
 FROM	festivalartists fa JOIN festivals f ON fa.festivalid = f.festivalid
-		  JOIN artists a ON fa.artistid = a.artistid
+          JOIN artists a ON fa.artistid = a.artistid
 WHERE	a.name IN ('%s')""" % ("','".join([str(i) for i in args])))
 
 #    print cur.query
-    
+
     festivals = cur.fetchall()
-    
+
     festivalList = []
-    
+
     for festival in festivals:
         festivalList.append({ "type": "Feature",
                              "geometry": {"type": "Point", "coordinates": [festival[0], festival[1]]},
-                             "properties": {"name": festival[2], "startDate": festival[3], "endDate": festival[4], "artists": festival[5]}})  
-    
+                             "properties": {"name": festival[2], "startDate": festival[3], "endDate": festival[4], "artists": festival[5]}})
+
     return jsonify(type = 'FeatureCollection', features = festivalList)
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# FestivalMapAPI
+## FestivalFinder
+
+#### Python Package list
+* flask  
+* flask_cors  
+* psycopg2  
+* python-dotenv
+* virtualenv
+
+#### How to run app
+* Ensure you have Homebrew installed
+* Ensure you have pip installed with `$ brew install pip`
+* Ensure you have virtualenv installed with `$ pip install virtualenv`
+* environment variables are set in a .env file in the root directory. It is formatted `VARIABLE_1=VALUE_OF_VARIABLE1`  
+* run `$ virtualenv --no-site-packages --distribute -p python3 virtualPy && source virtualPy/bin/activate && pip install -r requirements.txt` to setup virtualenv and install the required packages from requirements.txt
+* run `$ export FLASK_APP=festivalMapAPI.py` to set the flask env variable to tell flask where the app is
+* run `$ export FLASK_DEBUG=1` to set the flask server to debug mode with full logging & debug capability & auto-reload on change of python files
+* run `$ flask run` to launch the app
+
+
+// virtualenv -p python3 virtualPy
